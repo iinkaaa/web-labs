f7205ad0c5b7451260f66a3a54e664713f4af9c6
inka <ashurova.2005@mail.ru>
2025-06-06 13:42:19 +0300

    lab6

---
+++ lab6/app/config.py
@@ -0,0 +1,9 @@
+import os
+
+SECRET_KEY = 'secret-key'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///project.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media', 'images')
